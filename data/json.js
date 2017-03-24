0aecd6ea42acfd8924940c27e6dfade792203c1b
luisgomezipl <luisgomezip@gmail.com>
2017-03-24 15:51:54 +0000

    Data file

---
+++ data/json.js
@@ -0,0 +1,79 @@
+[
+  {
+    "name": "Luis Gomez",
+    "title": "Ing. XXX",
+    "photo": "",
+    "brief": "Desenvolvedor XXX",
+    "social": {
+      "facebook": "link",
+      "linkedin": "link",
+      "twitter": "link"
+    },
+    "experience": [
+      {
+        "business": "Business 1",
+        "job": "job 1",
+        "activities": "activities 1",
+        "tags": "tag1, tag2"
+      },
+      {
+        "business": "Business 2",
+        "job": "job 2",
+        "activities": "activities 2",
+        "tags": "tag1, tag2"
+      }
+    ],
+    "education": [
+      {
+        "place": "place 1",
+        "title": " title 1",
+        "level": "level 1",
+        "tags": "tag1, tag2"
+      },
+      {
+        "place": "place 2",
+        "title": " title 2",
+        "level": "level 2",
+        "tags": "tag1, tag2"
+      }
+    ],
+    "projects": [
+      {
+        "name": "pj 1",
+        "place": "place 1",
+        "description": "description 1",
+        "tags": "tag1, tag2"
+      },
+      {
+        "name": "pj 2",
+        "place": "place 2",
+        "description": "description 2",
+        "tags": "tag1, tag2"
+      }
+    ],
+    "certifications": [
+      {
+        "title": "title 1",
+        "description": "description 1",
+        "tags": "tag1, tag2"
+      },
+      {
+        "title": "title 1",
+        "description": "description 2",
+        "tags": "tag1, tag2"
+      }
+    ],
+    "awards": [
+      {
+        "title": "title 1",
+        "description": "description 1",
+        "tags": "tag1, tag2"
+      },
+      {
+        "title": "title 1",
+        "description": "description 2",
+        "tags": "tag1, tag2"
+      }
+    ]
+  }
+]
